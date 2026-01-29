fc3b56fd5cda543947c1b250773d90dd65dc51e4
SanikaKamble <sankam2005@gmail.com>
2026-01-29 22:51:27 +0530

    Added Task.java and updated AddTask module

--- src/AddTaskTest.java
+++ src/AddTaskTest.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
-
 public class AddTaskTest {
 
     public static void main(String[] args) {
 
+        // Use AddTask class (not AddTaskModule)
         AddTask addTaskModule = new AddTask();
-        Scanner sc = new Scanner(System.in);
 
-        // Test Case 1: Add a normal task
+        // Test Case 1: Add tasks
         addTaskModule.addTask("Learn Java");
         addTaskModule.addTask("Complete DevOps project");
 
+        // Display tasks after adding two tasks
         System.out.println("\nTasks after adding two tasks:");
-        addTaskModule.showTasks();
+        for (Task t : addTaskModule.getTasks()) {
+            System.out.println(t);
+        }
 
         // Test Case 2: Add empty task
         addTaskModule.addTask(""); // should print error
@@ -20,9 +21,10 @@ public class AddTaskTest {
         // Test Case 3: Add another task
         addTaskModule.addTask("Prepare for interviews");
 
+        // Display tasks after adding another task
         System.out.println("\nTasks after adding another task:");
-        addTaskModule.showTasks();
-
-        sc.close();
+        for (Task t : addTaskModule.getTasks()) {
+            System.out.println(t);
+        }
     }
 }
